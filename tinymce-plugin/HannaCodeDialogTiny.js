85c5187e1e141421b093e1592923cc7b5b9bcf94
phlppschrr <git@rgb10.de>
2026-01-21 15:29:02 +0100

    fix: add inline style to force FontAwesome font-family

--- tinymce-plugin/HannaCodeDialogTiny.js
+++ tinymce-plugin/HannaCodeDialogTiny.js
@@ -105,8 +105,8 @@ tinymce.PluginManager.add('HannaCodeDialogTiny', function (editor, url) {
     // Support both FontAwesome icon names (e.g., 'sun-o') and SVG URLs
     var iconName = 'hcdtdd';
     if (hcdt_config.dropdown_icon.indexOf('/') === -1 && hcdt_config.dropdown_icon.indexOf('.') === -1) {
-        // It's a FontAwesome icon name, register as HTML
-        editor.ui.registry.addIcon(iconName, '<i class="fa fa-' + hcdt_config.dropdown_icon + '"></i>');
+        // It's a FontAwesome icon name, register as HTML with inline style
+        editor.ui.registry.addIcon(iconName, '<i class="fa fa-' + hcdt_config.dropdown_icon + '" style="font-family: FontAwesome !important;"></i>');
     } else {
         // It's a URL, register as custom icon
         editor.ui.registry.addIcon(iconName, '<img style="width:24px; height: 24px;" src="' + hcdt_config.dropdown_icon + '" />');
